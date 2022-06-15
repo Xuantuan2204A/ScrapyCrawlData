4196d96f3e143233fe80f9276a2c65d1729e91e4
Xuantuan2204A <105768116+Xuantuan2204A@users.noreply.github.com>
2022-06-15 17:38:57 +0700

    update data elastic

---
+++ CrawldataDb/CrawldataDb/spiders/query_elastic.py
@@ -0,0 +1,24 @@
+#-*- coding: utf-8 -*-
+from elasticsearch import Elasticsearch
+# from elasticsearch_dsl import Search
+
+es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+# search = Search(using=ES, index="scrapy")
+
+# for hit in search.scan():
+#     name = hit['category_name'];
+#     print(name.encode('utf-8'))
+q = {
+  "script": {
+    "source": "ctx._source.category_name='life style'",
+    "lang": "painless"
+  },
+  "query": {
+    "match": {
+        "category_url": "https://thegioihoinhap.vn/category/loi-song/"
+    }
+  }
+}
+
+es.update_by_query(body=q, doc_type='doc_name', index='database_index')
+   
