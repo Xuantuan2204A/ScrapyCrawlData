42802ab5ab274009ac635aa252d2d5f659425f93
Xuantuan2204A <105768116+Xuantuan2204A@users.noreply.github.com>
2022-06-17 17:38:15 +0700

    update data elasticsearch

--- CrawldataDb/CrawldataDb/spiders/query_elastic.py
+++ CrawldataDb/CrawldataDb/spiders/query_elastic.py
@@ -1,24 +1,53 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+__author__ = 'XUANTUAN'
+
 from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Search
 
-es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-search = Search(using=es, index="database_index")
 
-for hit in search.scan():
-    name = hit['category_name'];
-    print(name.encode('utf-8'))
-q = {
-  "script": {
-    "source": "ctx._source.category_name='life style'",
-    "lang": "painless"
-  },
-  "query": {
-    "match": {
-        "category_url": "https://thegioihoinhap.vn/category/loi-song/"
-    }
-  }
-}
+class GetSpider():
+    def __init__(self, *args, **kwargs):
+        self.es_doc_type = "fb"
+        self.es_index = "database_scrapy"
+        self.es = Elasticsearch("http://127.0.0.1:9200")
 
-es.update_by_query(body=q, doc_type='doc_name', index='database_index')
-   
+    def startGet(self):
+        page = 1
+        limit = 10
+        while True:
+            print("\n------------------------")
+            print("#Page: " + str(page) + "\n")
+
+            begin = ((page-1)*limit)
+            # self.es.indices.refresh(index=self.es_index)
+            docs = {
+                "_source": ["category_url","createdate","category_name"],
+                "query": {
+                    "bool" : {
+                        "filter" : {
+                            "bool" : {
+                                "must" : [
+                                    {"term" : { "category_url" : "b1d49da1b7fc9eecce0bce89efe5dda7" }},
+                                ]
+                            }
+                        }
+                    }
+                },
+                # "query": {
+                #    "match_all":{}
+                # },
+                "size": limit,
+                "from": begin
+            }
+            res = self.es.search(index=self.es_index, body=docs)
+            if(len(res['hits']['hits']) == 0):
+                break
+        
+            for hit in res['hits']['hits']:
+                id = hit["_id"]
+                print(id) 
+                self.es.update(index=self.es_index , id=id, body=dict(
+                    {'doc': {'category_name': "Agriculture"}}))
+            page = page + 1
+
+getdata = GetSpider()
+getdata.startGet()
